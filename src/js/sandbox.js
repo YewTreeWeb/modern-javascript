80f283b81f6f7eeed12ee956a1014b5cd9c3a57c
Mathew Teague <mathew.teague@yewtreeweb.co.uk>
2020-01-05 10:02:15 +0000

    section 4

--- src/js/sandbox.js
+++ src/js/sandbox.js
@@ -97,6 +97,100 @@ console.log(age === 30);
     === strict comparison
 */
 
+/* Loops */
+let i = 5;
+
+// for (i; i < 5; i++) {
+// 	console.log('in for loop: ', i);
+// }
+
+// while (i < 5) {
+// 	console.log('while loop number: ', i);
+// 	i++;
+// }
+
+do {
+	// can run the while loop just once even if not true.
+	console.log('val of i is: ', i);
+	i++;
+} while (i < 5);
+
+/* if statements */
+const password = 'p@assword';
+
+if (password.length >= 12 && password.includes('@')) {
+	console.log('This is a great password.');
+} else if (password.length >= 8) {
+	console.log('Password is the right length.');
+} else {
+	console.error('Password is too short!');
+}
+
+/* Logical operators 
+    && = and, if both are true
+    || = or, only one condition has to be true
+    ! = not true, do if is false
+    !var = switch value to true/false
+*/
+
+/* Break & continue */
+
+const scores = [ 50, 25, 0, 100, 20, 40 ];
+
+for (let i = 0; i < scores.length; i++) {
+	if (scores[i] === 0) {
+		continue;
+	}
+
+	console.log('Your score is: ', scores[i]);
+
+	if (scores[i] === 100) {
+		console.log('You got to a 100!');
+		break;
+	}
+}
+
+/* Switch statements */
+
+const grade = 'D';
+// Checks with === and same type as created var.
+switch (grade) {
+	case 'A':
+		console.log('Great stuff you got an A!');
+		break;
+	case 'B':
+		console.log('Well done you got an B.');
+		break;
+	case 'C':
+		console.log('Well done you got an C');
+		break;
+	case 'D':
+		console.log('Ah you got a D. Better luck next time.');
+		break;
+	case 'E':
+		console.log('Oh dear got a E. Better luck next time.');
+		break;
+	case 'F':
+		console.log('What happened? You failed!');
+		break;
+	default:
+		console.error('Not a vaild grade.');
+}
+
+/* Funtions */
+
+// Function declaration
+function greet() {
+	console.log('Hello');
+}
+greet(); // Can call above function.
+
+// Function Expression
+const speak = function() {
+	console.log('Good day');
+};
+speak(); // Cannot call before function
+
 /* Define default vars */
 const html = document.getElementsByTagName('html')[0];
 const body = document.getElementsByTagName('body')[0];
