6d194028b8bfdc9180f092f4309dbc821d6c68b3
Mathew Teague <mathew.teague@outlook.com>
2020-01-25 21:01:06 +0000

    learning objects

--- src/js/sandbox.js
+++ src/js/sandbox.js
@@ -110,20 +110,20 @@ let i = 5;
 // }
 
 do {
-	// can run the while loop just once even if not true.
-	console.log('val of i is: ', i);
-	i++;
+  // can run the while loop just once even if not true.
+  console.log('val of i is: ', i);
+  i++;
 } while (i < 5);
 
 /* if statements */
 const password = 'p@assword';
 
 if (password.length >= 12 && password.includes('@')) {
-	console.log('This is a great password.');
+  console.log('This is a great password.');
 } else if (password.length >= 8) {
-	console.log('Password is the right length.');
+  console.log('Password is the right length.');
 } else {
-	console.error('Password is too short!');
+  console.error('Password is too short!');
 }
 
 /* Logical operators 
@@ -135,19 +135,19 @@ if (password.length >= 12 && password.includes('@')) {
 
 /* Break & continue */
 
-const scores = [ 50, 25, 0, 100, 20, 40 ];
+const scores = [50, 25, 0, 100, 20, 40];
 
 for (let i = 0; i < scores.length; i++) {
-	if (scores[i] === 0) {
-		continue;
-	}
+  if (scores[i] === 0) {
+    continue;
+  }
 
-	console.log('Your score is: ', scores[i]);
+  console.log('Your score is: ', scores[i]);
 
-	if (scores[i] === 100) {
-		console.log('You got to a 100!');
-		break;
-	}
+  if (scores[i] === 100) {
+    console.log('You got to a 100!');
+    break;
+  }
 }
 
 /* Switch statements */
@@ -155,45 +155,45 @@ for (let i = 0; i < scores.length; i++) {
 const grade = 'D';
 // Checks with === and same type as created var.
 switch (grade) {
-	case 'A':
-		console.log('Great stuff you got an A!');
-		break;
-	case 'B':
-		console.log('Well done you got an B.');
-		break;
-	case 'C':
-		console.log('Well done you got an C');
-		break;
-	case 'D':
-		console.log('Ah you got a D. Better luck next time.');
-		break;
-	case 'E':
-		console.log('Oh dear got a E. Better luck next time.');
-		break;
-	case 'F':
-		console.log('What happened? You failed!');
-		break;
-	default:
-		console.error('Not a vaild grade.');
+  case 'A':
+    console.log('Great stuff you got an A!');
+    break;
+  case 'B':
+    console.log('Well done you got an B.');
+    break;
+  case 'C':
+    console.log('Well done you got an C');
+    break;
+  case 'D':
+    console.log('Ah you got a D. Better luck next time.');
+    break;
+  case 'E':
+    console.log('Oh dear got a E. Better luck next time.');
+    break;
+  case 'F':
+    console.log('What happened? You failed!');
+    break;
+  default:
+    console.error('Not a vaild grade.');
 }
 
 /* Funtions */
 
 // Function declaration
 function greet() {
-	console.log('Hello');
+  console.log('Hello');
 }
 greet(); // Can call above function.
 
 // Function Expression
-const speak = function() {
-	console.log('Good day');
+const speak = function () {
+  console.log('Good day');
 };
 speak(); // Cannot call before function
 
 // Arguments & Parameters
 const speak2 = (name = 'Bob', day = 'morning') => {
-	console.log(`good ${day} ${name}`);
+  console.log(`good ${day} ${name}`);
 };
 speak2('Naruto', 'night');
 
@@ -203,15 +203,65 @@ const area = calc(5);
 console.log('area is: ' + area);
 
 // Foreach Methods & Callbacks
-const people = [ 'mark', 'henry', 'george', 'lenny', 'hugo', 'jekyll', 'hyde' ];
+const people = ['mark', 'henry', 'george', 'lenny', 'hugo', 'jekyll', 'hyde'];
 
 const ul = document.querySelector('.testlist');
 let html = ``;
 
 people.forEach((person) => {
-	html += `<li class="testlist__item">${person.toLowerCase()}</li>`;
+  html += `<li class="testlist__item">${person.toLowerCase()}</li>`;
 });
 ul.innerHTML = html;
+
+/* Objects */
+
+let user = {
+  name: 'Mat',
+  age: 30,
+  email: 'mathew.teageu@outlook.com',
+  location: 'Birmingham',
+  blogs: [{
+      title: 'Hello',
+      likes: 20
+    },
+    {
+      title: 'World',
+      likes: 40
+    }
+  ],
+  login: () => console.log('user logged in!'),
+  logout: () => console.log('user logged out!'),
+  logBlogs() { // if set the => the 'this' will be set to the global function not within the user object.
+    this.blogs.forEach(blog => {
+      console.log(blog.title, blog.likes);
+    }); // this referes to the user object.
+  }
+};
+
+console.log(user);
+console.log(user.name);
+console.log(user['email']);
+
+user['name'] = 'Bob';
+console.log(user['name']);
+
+const key = 'location';
+console.log(user[key]);
+
+console.log(typeof user);
+
+user.login();
+
+user.logBlogs();
+
+const number = 7.4;
+console.log(Math.round(number));
+console.log(Math.floor(number));
+console.log(Math.ceil(number));
+console.log(Math.trunc(number));
+
+const random = Math.random();
+console.log(Math.round(random * 100))
 
 /* Define default vars */
 const body = document.getElementsByTagName('body')[0];
