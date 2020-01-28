7bfbcd57873bd3fbf9f9d4761e50fbad255c5be3
Mathew Teague <mathew.teague@yewtreeweb.co.uk>
2020-01-28 21:55:31 +0000

    grabbed all text elements with certain text and applied a class

--- src/js/modules/helpers.js
+++ src/js/modules/helpers.js
@@ -40,35 +40,3 @@
 		}
 	}
 })();
-
-// Fallback for font-display, once the API knows a font has loaded, we can apply a class of fonts-loaded to the <html> tag.
-(function() {
-	try {
-		const e = document.createElement('style');
-		e.textContent = '@font-face { font-display: swap; }';
-		document.documentElement.appendChild(e);
-		var isFontDisplaySupported = e.sheet.cssRules[0].cssText.indexOf('font-display') !== -1;
-		e.remove();
-	} catch (e) {}
-
-	if (isFontDisplaySupported === false && 'fonts' in document) {
-		document.fonts.load('1em Open Sans Regular');
-		document.fonts.ready.then(function(fontFaceSet) {
-			document.documentElement.className += ' fonts-loaded';
-		});
-	} else {
-		// Maybe figure out your own strategy, but this might be sensible:
-		document.documentElement.className += ' fonts-loaded';
-	}
-})();
-
-// Add tabbing indicator to window
-function handleFirstTab(e) {
-	if (e.keyCode === 9) {
-		// the "I am a keyboard user" key
-		document.body.classList.add('user-is-tabbing');
-		window.removeEventListener('keydown', handleFirstTab);
-	}
-}
-
-window.addEventListener('keydown', handleFirstTab);
--- src/js/sandbox.js
+++ src/js/sandbox.js
@@ -110,20 +110,20 @@ let i = 5;
 // }
 
 do {
-  // can run the while loop just once even if not true.
-  console.log('val of i is: ', i);
-  i++;
+	// can run the while loop just once even if not true.
+	console.log('val of i is: ', i);
+	i++;
 } while (i < 5);
 
 /* if statements */
 const password = 'p@assword';
 
 if (password.length >= 12 && password.includes('@')) {
-  console.log('This is a great password.');
+	console.log('This is a great password.');
 } else if (password.length >= 8) {
-  console.log('Password is the right length.');
+	console.log('Password is the right length.');
 } else {
-  console.error('Password is too short!');
+	console.error('Password is too short!');
 }
 
 /* Logical operators 
@@ -135,19 +135,19 @@ if (password.length >= 12 && password.includes('@')) {
 
 /* Break & continue */
 
-const scores = [50, 25, 0, 100, 20, 40];
+const scores = [ 50, 25, 0, 100, 20, 40 ];
 
 for (let i = 0; i < scores.length; i++) {
-  if (scores[i] === 0) {
-    continue;
-  }
+	if (scores[i] === 0) {
+		continue;
+	}
 
-  console.log('Your score is: ', scores[i]);
+	console.log('Your score is: ', scores[i]);
 
-  if (scores[i] === 100) {
-    console.log('You got to a 100!');
-    break;
-  }
+	if (scores[i] === 100) {
+		console.log('You got to a 100!');
+		break;
+	}
 }
 
 /* Switch statements */
@@ -155,45 +155,45 @@ for (let i = 0; i < scores.length; i++) {
 const grade = 'D';
 // Checks with === and same type as created var.
 switch (grade) {
-  case 'A':
-    console.log('Great stuff you got an A!');
-    break;
-  case 'B':
-    console.log('Well done you got an B.');
-    break;
-  case 'C':
-    console.log('Well done you got an C');
-    break;
-  case 'D':
-    console.log('Ah you got a D. Better luck next time.');
-    break;
-  case 'E':
-    console.log('Oh dear got a E. Better luck next time.');
-    break;
-  case 'F':
-    console.log('What happened? You failed!');
-    break;
-  default:
-    console.error('Not a vaild grade.');
+	case 'A':
+		console.log('Great stuff you got an A!');
+		break;
+	case 'B':
+		console.log('Well done you got an B.');
+		break;
+	case 'C':
+		console.log('Well done you got an C');
+		break;
+	case 'D':
+		console.log('Ah you got a D. Better luck next time.');
+		break;
+	case 'E':
+		console.log('Oh dear got a E. Better luck next time.');
+		break;
+	case 'F':
+		console.log('What happened? You failed!');
+		break;
+	default:
+		console.error('Not a vaild grade.');
 }
 
 /* Funtions */
 
 // Function declaration
 function greet() {
-  console.log('Hello');
+	console.log('Hello');
 }
 greet(); // Can call above function.
 
 // Function Expression
-const speak = function () {
-  console.log('Good day');
+const speak = function() {
+	console.log('Good day');
 };
 speak(); // Cannot call before function
 
 // Arguments & Parameters
 const speak2 = (name = 'Bob', day = 'morning') => {
-  console.log(`good ${day} ${name}`);
+	console.log(`good ${day} ${name}`);
 };
 speak2('Naruto', 'night');
 
@@ -203,39 +203,40 @@ const area = calc(5);
 console.log('area is: ' + area);
 
 // Foreach Methods & Callbacks
-const people = ['mark', 'henry', 'george', 'lenny', 'hugo', 'jekyll', 'hyde'];
+const people = [ 'mark', 'henry', 'george', 'lenny', 'hugo', 'jekyll', 'hyde' ];
 
 const ul = document.querySelector('.testlist');
 let html = ``;
 
 people.forEach((person) => {
-  html += `<li class="testlist__item">${person.toLowerCase()}</li>`;
+	html += `<li class="testlist__item">${person.toLowerCase()}</li>`;
 });
 ul.innerHTML = html;
 
 /* Objects */
 
 let user = {
-  name: 'Mat',
-  age: 30,
-  email: 'mathew.teageu@outlook.com',
-  location: 'Birmingham',
-  blogs: [{
-      title: 'Hello',
-      likes: 20
-    },
-    {
-      title: 'World',
-      likes: 40
-    }
-  ],
-  login: () => console.log('user logged in!'),
-  logout: () => console.log('user logged out!'),
-  logBlogs() {
-    // Method instead of object.
-    // if set the => the 'this' will be set to the global function not within the user object.
-    this.blogs.forEach(blog => console.log(`Blog title is: ${blog.title}. Number of likes are: ${blog.likes}`)); // this referes to the user object - user. e.g. this user.
-  }
+	name: 'Mat',
+	age: 30,
+	email: 'mathew.teageu@outlook.com',
+	location: 'Birmingham',
+	blogs: [
+		{
+			title: 'Hello',
+			likes: 20
+		},
+		{
+			title: 'World',
+			likes: 40
+		}
+	],
+	login: () => console.log('user logged in!'),
+	logout: () => console.log('user logged out!'),
+	logBlogs() {
+		// Method instead of object.
+		// if set the => the 'this' will be set to the global function not within the user object.
+		this.blogs.forEach((blog) => console.log(`Blog title is: ${blog.title}. Number of likes are: ${blog.likes}`)); // this referes to the user object - user. e.g. this user.
+	}
 };
 
 console.log(user);
@@ -261,7 +262,7 @@ console.log(Math.ceil(number));
 console.log(Math.trunc(number));
 
 const random = Math.random();
-console.log(Math.round(random * 100))
+console.log(Math.round(random * 100));
 
 /* Define default vars */
 const body = document.getElementsByTagName('body')[0];
@@ -276,6 +277,73 @@ const paras = document.querySelectorAll('.content__bottom p'); // Selects all p 
 console.log(para);
 console.log(paras[1]);
 
-paras.forEach(para => console.log(para));
+paras.forEach((para) => console.log(para));
 
 const contents = document.getElementsByClassName('content'); // Creates a HTML collection. // This is not the same as a node list such as querySelectorAll. Can't do forEach loops with collections.
+
+const link = document.querySelector('.content__bottom a');
+console.log(`Current link href: ${link.getAttribute('href')}`);
+link.setAttribute('href', 'https://yewtreeweb.co.uk');
+link.innerText = 'YewTreeWeb';
+console.log(`New link href: ${link.getAttribute('href')}`);
+link.setAttribute('style', 'color: orange;');
+
+// Get style property
+console.log(para.style.color); // Get's the color of the element.
+para.style.fontSize = '20px'; // Add style instead of setAttribute('style', 'color: orange;');
+para.style.color = ''; // Removes style.
+
+const siteTitle = document.querySelector('h1');
+console.log(siteTitle.classList);
+siteTitle.classList.add('title--lg');
+// siteTitle.classList.remove('title--lg');
+
+/* Grabs all p tags and checks if they have the words success or error and then apply the correct class to them. */
+const paraTags = document.querySelectorAll('.content__middle p');
+paraTags.forEach((paraTag) => {
+	try {
+		// If use innerText all visible text within element is got. So no text within a sub element.
+		// To get all text use textContent.
+		const errorTag = paraTag.textContent.includes('error');
+		const successTag = paraTag.textContent.includes('success');
+		if (errorTag) {
+			paraTag.classList.add('error');
+		}
+		if (successTag) {
+			paraTag.classList.add('success');
+		}
+	} catch (e) {
+		console.error(e);
+		console.info(paraTag);
+	}
+});
+
+// Manipluate children.
+const article = document.querySelector('article');
+console.log(article.children);
+
+// Array.from turns collection into an array.
+Array.from(article.children).forEach((child) => child.classList.add('article-element'));
+
+// Get parent
+const articleTitle = document.querySelector('article h2');
+console.log(articleTitle.parentElement);
+
+// Get siblings
+console.log(articleTitle.nextElementSibling);
+console.log(articleTitle.previousElementSibling);
+
+/* Basic Events */
+
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+	console.log('You clicked me.');
+});
+
+const listItems = document.querySelectorAll('.todoList li');
+listItems.forEach((item) => {
+	item.addEventListener('click', (el) => {
+		console.log(el.target);
+		el.target.style.textDecoration = 'line-through';
+	});
+});
